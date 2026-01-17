63facf5baab6859ed103a7a4b4e62bef760c9b33
AzureWind <wyyou90@gmail.com>
2026-01-18 02:52:32 +0800

    增加了大量内容。Level2Vk的两个mp还需要手动创建。

--- idvPianoTool/files.h
+++ idvPianoTool/files.h
@@ -2,17 +2,68 @@
 #include <iostream>
 #include <windows.h>
 #include <vector>
+#include <map>
 #include <string>
 #include <locale>
 #include <codecvt>
+#include <set>
+#include <fstream>
+#include <cstdlib>
+#include "json.hpp"
 int print2hd(HWND hd, std::wstring in);
 
 std::wstring s2ws(const std::string& str);
 
+#pragma pack(push, 1)
 class KeyInput {
 public:
 	DWORD dwFlag;
 	WORD wVk;
+	double delay; //ms
+	static bool compare(const KeyInput& a, const KeyInput& b) {
+		return a.delay < b.delay;
+	}
+	void printKeyInput(void) {
+		std::cout << dwFlag << ' ' <<  wVk << ' ' << delay << '\n';
+	}
+};
+#pragma pack(pop)
+
+class Level2Vk {
+public:
+	static int mode;
+	static std::map<std::pair<char, int>, WORD> mp1;
+	static std::map<std::pair<char, int>, WORD> mp2;
+	static WORD toVk(const char& level, const int& scale) {
+		if (mode == 1) {
+			return mp1[{level, scale}];
+		}
+		else if (mode == 2) {
+			return mp2[{level, scale}];
+		}
+		else {
+			return 0;
+		}
+	}
+};
+
+class Note {
+public:
+	char level;
+	int scale;
+	double start;
+	double end;
+	static std::set<int> scales;
+	Note(std::string l, double s, double e) {
+		level = l[0];
+		scale = l[1];
+		start = s;
+		end = e;
+		scales.insert(scale);
+	}
+	void printNote(void) {
+		std::cout << level << ' ' << scale << ' ' << start << ' ' << end << '\n';
+	}
 };
 
 class KeyList {
@@ -23,12 +74,26 @@ public:
 		KeyL.push_back(a);
 		return KeyL.size();
 	}
-	int pushInput(WORD a, DWORD b) {
+	int pushInput(WORD a, DWORD b, double c) {
 		KeyInput tmp;
 		tmp.dwFlag = b;
 		tmp.wVk = a;
+		tmp.delay = c;
 		KeyL.push_back(tmp);
+		//std::cout << "pushed :" << 
 		return KeyL.size();
+	}
+	int pushInput(Note& note) {
+		this->pushInput(Level2Vk::toVk(note.level, note.scale), 0, note.start);
+		return this->pushInput(Level2Vk::toVk(note.level, note.scale), KEYEVENTF_KEYUP, note.end);
+	}
+	void sortself(void) {
+		std::sort(KeyL.begin(), KeyL.end(), KeyInput::compare);
+	}
+	void printKeyList(void) {
+		for (auto& i : KeyL) {
+			i.printKeyInput();
+		}
 	}
 	int print2hd(HWND hd) {
 		if (!IsWindow(hd)) {
@@ -38,17 +103,26 @@ public:
 		SetForegroundWindow(hd);
 		std::cout << "opened\n";
 		Sleep(100);
+		LARGE_INTEGER Fre, end;
+		QueryPerformanceFrequency(&Fre);
 		INPUT A;
 		ZeroMemory(&A, sizeof(INPUT));
 		A.type = INPUT_KEYBOARD;
-		int cnt = 0;
-		for (auto i : KeyL) {
-			A.ki.wVk = i.wVk;
+		LARGE_INTEGER TimeStart;
+		double deltaT = 0;
+		timeBeginPeriod(1);
+		QueryPerformanceCounter(&TimeStart);
+		for (auto& i : KeyL) {
 			A.ki.dwFlags = i.dwFlag;
+			A.ki.wVk = i.wVk;
+			QueryPerformanceCounter(&end);
+			deltaT = i.delay - 1.0 * (end.QuadPart - TimeStart.QuadPart) / Fre.QuadPart;
+			if (deltaT > 1) {
+				Sleep(deltaT);
+			}
 			SendInput(1, &A, sizeof(INPUT));
-			cnt++;
-			Sleep(70);
 		}
-		return cnt;
+		timeEndPeriod(1);
 	}
 };
+
--- idvPianoTool/idvPianoTool.cpp
+++ idvPianoTool/idvPianoTool.cpp
@@ -1,11 +1,15 @@
 ﻿#pragma once
-
 #include <windows.h>
 #include <string>
 #include <iostream>
 #include "files.h"
+#include "json.hpp"
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
 //main1
-int main1() {
+using json = nlohmann::json;
+std::string path = "C:\\Users\\Moonlight\\Desktop";
+/*int main1() {
 	std::string a;
 	wchar_t hdName[] = L"新建文本文档.txt - 记事本";
 	std::cout << "input a char string here:\n";
@@ -33,7 +37,7 @@ int main1() {
 		}
 	}
 	return 0;
-}
+}*/
 //main2
 int main2() {
 	INPUT a;
@@ -55,14 +59,153 @@ int main2() {
 	return 0;
 }
 //main3
-int main() {
+int main3() {
 	SetProcessDPIAware();
 	INPUT a;
+	HWND hd;
 	ZeroMemory(&a, sizeof(INPUT));
 	a.type = INPUT_MOUSE;
 	int WindowWidth = GetSystemMetrics(SM_CXSCREEN);
-	int WindowLength = GetSystemMetrics(SM_CYSCREEN);
-	std::cout << WindowWidth << " " << WindowLength;
+	int WindowHeight = GetSystemMetrics(SM_CYSCREEN);
+	int inX, inY;
+	int outX, outY;
+	while (1) {
+		std::cout << "Enter 2 nunbers (-1 to break, -2 to click):\n";
+		std::cin >> inX;
+		if (inX == -1)break;
+		if (inX == -2) {
+			a.mi.dwFlags = MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP;
+			SendInput(1, &a, sizeof(INPUT));
+			continue;
+		}
+		std::cin >> inY;
+		outX = inX * 65535 / (WindowWidth - 1);
+		outY = inY * 65535 / (WindowHeight - 1);
+		std::cout << outX << ' ' << outY << '\n';
+		//hd = FindWindowW(nullptr, L"新建文本文档.txt - 记事本");
+		a.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+		a.mi.dx = outX;
+		a.mi.dy = outY;
+		SendInput(1, &a, sizeof(INPUT));
+	}
+	return 0;
+}
+//main4
+int main4() {
+	RegisterHotKey(nullptr, 999, MOD_ALT | MOD_NOREPEAT, 'S');
+	RegisterHotKey(nullptr, 1000, MOD_ALT | MOD_NOREPEAT, 'Q');
+	MSG msg;
+	while (1) {
+		std::cout << "loop in\n";
+		GetMessage(&msg, nullptr, 0, 0);
+		std::cout << "here\n";
+		if (msg.message == WM_HOTKEY) {
+			if (msg.wParam == 999) {
+				std::cout << "Status : Active\n";
+			}
+			else if (msg.wParam == 1000) {
+				std::cout << "Leaving\n";
+				Sleep(700);
+				UnregisterHotKey(nullptr, 999);
+				UnregisterHotKey(nullptr, 1000);
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
+//main5
+int main5() {
+	RegisterHotKey(nullptr, 100, MOD_ALT, 'Q');
+	RegisterHotKey(nullptr, 101, MOD_ALT, 'S');
+	LARGE_INTEGER tot;
+	QueryPerformanceFrequency(&tot);
+	MSG msg;
+	timeBeginPeriod(1);
+
+	while (1) {
+		GetMessage(&msg, nullptr, 0, 0);
+		if (msg.wParam == 100) {
+			std::cout << "Leaving\n";
+			Sleep(700);
+			return 0;
+		}
+		else {
+			std::cout << "input a sleeping number(ms) :\n";
+			int a;
+			std::cin >> a;
+			LARGE_INTEGER begin, end;
+			QueryPerformanceCounter(&begin);
+			Sleep(a);
+			QueryPerformanceCounter(&end);
+			std::cout << "begin: " << begin.QuadPart << ", end: " << end.QuadPart << '\n';
+			std::cout << "total seconds:" << 1.0 * (end.QuadPart - begin.QuadPart) / tot.QuadPart;
+		}
+	}
+	timeEndPeriod(1);
+	UnregisterHotKey(nullptr, 100);
+	UnregisterHotKey(nullptr, 101);
+	return 0;
+}
+//main6
+int reachfile(const std::string& command, const std::string& targetpath, json& j) {
+	int flag = std::system(command.c_str());
+	if (flag == 1) {
+		std::cerr << "flp转换失败.\n";
+		return -1;
+	}
+	std::ifstream ijson(targetpath);
+	if (!ijson.is_open()) {
+		std::cerr << "Can't open file: " << targetpath << '\n';
+		return -1;
+	}
+	try {
+		ijson >> j;
+	}
+	catch (json::parse_error& e) {
+		std::cerr << "Parse_error: " << e.what() << '\n';
+		return -1;
+	}
+	ijson.close();
+	return 0;
+}
+
+std::set<int> Note::scales;
+std::vector<Note> NoteList;
+int Level2Vk::mode = 1;
+std::map<std::pair<char, int>, WORD> Level2Vk::mp1;
+std::map<std::pair<char, int>, WORD> Level2Vk::mp2;
+int main() {
+	
+	std::string exporterpath = "flp_exporter.exe";
+	std::string outpath = "out.json";
+
+	std::cout << "input your target file:\n";
+	std::string get;
+	std::cin >> get;
+	json j;
+	if (reachfile((exporterpath + ' ' + get + ' ' + outpath).c_str(), outpath, j) != 0) {
+		std::cerr << "程序已停止运行\n";
+		return -1;
+	}
+
+	KeyList keys;
+
+	//print all json contents;
+	for (int i = 0; i < j.size(); i++) {
+		std::cout << j[i]["pitch"] << ' ' << j[i]["start_ms"] << ' ' << j[i]["end_ms"] << '\n';
+	}
+
+	for (int i = 0; i < j.size(); i++) {
+		Note tmp(j[i]["pitch"], j[i]["start_ms"], j[i]["end_ms"]);
+		keys.pushInput(tmp);
+	}
+	std::cout << "--------\n";
+	keys.printKeyList();
+	std::cout << "-------\n";
+	keys.sortself();
+	keys.printKeyList();
+	return 0;
 }
 /*
 int main() {
